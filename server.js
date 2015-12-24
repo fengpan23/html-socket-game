3804313aab2c4bddcc88746aa9d7e3a89b764784
fengpan <pan.feng@stargt.com.cn>
2015-12-24 18:49:04 +0800

    update

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+let webSocketServer = require('ws').Server
+let wss = new webSocketServer({ port: 8000 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+    ws.on('close', function close() {
+        console.log('disconnected');
+    });
+    ws.send('something');
+});
